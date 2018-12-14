b232a28ebe4bfcc699192913d98dee709686bbeb
gurvesh <guru01@gmail.com>
2018-12-14 21:48:08 +0530

    Update Day14.clj

--- Day14.clj
+++ Day14.clj
@@ -1,27 +1,28 @@
 (ns day14
     (:use [clojure.repl]))
     
-(defn next-tick [{:keys [i j n scores] :as full}]
+(defn next-tick [{:keys [i j scores] :as full}]
     (let [x (+ (scores i) (scores j))
+          n (count scores)
           new-scores (if (< x 10)
                          (assoc scores n x)
                          (assoc scores  n (int (/ x 10)), (inc n) (mod x 10)))
-          n' (if (< x 10)
-                 (inc n)
-                 (+ n 2))]
-        {:i (mod (inc (+ i (new-scores i))) n')
-         :j (mod (inc (+ j (new-scores j))) n')
-         :n n'
+          n (if (< x 10)
+                (inc n)
+                (+ n 2))]
+        {:i (mod (inc (+ i (new-scores i))) n)
+         :j (mod (inc (+ j (new-scores j))) n)
          :scores new-scores}))
          
-(def starting {:i 0, :j 1, :n 2, :scores {0 3, 1 7}})
+(def starting {:i 0, :j 1, :scores {0 3, 1 7}})
 
-(defn get-last-x [x {:keys [n scores] :as full}]
-    (let [s (map #(scores %) (range (- n x 1) n))]
+(defn get-last-x [x {:keys [scores] :as full}]
+    (let [n (count scores)
+          s (map #(scores %) (range (- n x 1) n))]
         (partition x 1 s)))
 
 ;; One of the 2 is the answers for the 1st part
-(time (doall (->> (drop-while #(< (:n %) (+ 10 637061)) (iterate next-tick starting))
+(time (doall (->> (drop-while #(< (count (:scores %)) (+ 10 637061)) (iterate next-tick starting))
                   first
                   (get-last-x 10))))
                   
@@ -39,7 +40,8 @@
 (time (->> (iterate next-tick starting)
            (drop-while #(not (match '(6 3 7 0 6 1) (get-last-x 6 %))))
            first
-           :n
+           :scores
+           count
            (#(- % 6))))
 
 ;; "Elapsed time: 175296.735223 msecs"
